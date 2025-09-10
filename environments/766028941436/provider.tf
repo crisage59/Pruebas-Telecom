909c3b8547f959348f6ff6d53f74d2b0daa531cb
cristopher sage <csage@teco.com.ar>
2025-09-10 11:59:01 -0300

    pruebas

--- environments/766028941436/provider.tf
+++ environments/766028941436/provider.tf
@@ -1,24 +1,32 @@
-# provider "aws" {
-#   region = "us-east-2"
-#   assume_role {
-#     role_arn = "arn:aws:iam::766028941436:role/terraform/sitiosweb-gitlab-deploy-role"
-#   }
-# }
+provider "aws" {
+  alias   = "virginia"
+  region  = "us-east-1"
+  access_key = "fake"
+  secret_key = "fake"
+}
 
-# provider "aws" {
-#   region = "us-east-1"
-#   alias  = "virginia"
-#   assume_role {
-#     role_arn = "arn:aws:iam::766028941436:role/terraform/sitiosweb-gitlab-deploy-role"
-#   }
-# }
+provider "aws" {
+  alias   = "ohio"
+  region  = "us-east-2"
+  access_key = "fake"
+  secret_key = "fake"
+}
 
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 5.0"
-#     }
-#   }
-# }
+
+provider "aws" {
+  region = "us-east-1"
+  alias  = "virginia"
+  assume_role {
+    role_arn = "arn:aws:iam::766028941436:role/terraform/sitiosweb-gitlab-deploy-role"
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
